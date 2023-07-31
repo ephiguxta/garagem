090f2eaaf7c4b755e321c8f6672a7db73260cf2a
giovanels <137007209+giovanels@users.noreply.github.com>
2023-07-31 07:56:48 -0400

    Prototipação da classe pessoa e suas heranças

---
+++ src/include/pessoa.py
@@ -0,0 +1,55 @@
+class Pessoa
+    def __init__(self, nome, endereco, numero, bairro, cep, telefone, email):
+        self._nome = nome
+        self._endereco = endereco
+        self._numero = numero
+        self._bairro = bairro
+        self._cep = cep
+        self._telefone = telefone
+        self._email = email
+
+#metodos getters e setters
+    def getNome(self):
+        return self._nome
+
+    def setNome(self, nome):
+        self._nome = nome
+
+    def getEndereco(self):
+        return self._endereco
+
+    def setEndereco(self, endereco):
+        self._endereco = endereco
+
+    def getNumero(self):
+        return self._numero
+
+    def setNumero(self, numero):
+        self._numero = numero
+
+    def getBairro(self):
+        return self._bairro
+
+    def setBairro(self, bairro):
+        self._bairro = bairro
+
+    def getCep(self):
+        return self._cep
+
+    def setCep(self, cep):
+        self._cep = cep
+
+    def getTelefone(self):
+        return self._telefone
+
+    def setTelefone(self, telefone):
+        self._telefone = telefone
+
+    def getEmail(self):
+        return self._email
+
+    def setEmail(self, email)
+        self._email = email
+
+    def pagar(self, preco):
+        pass
