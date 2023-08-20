d37688c08220bb3baa40d1c28bd404e3eae68d34
ephiguxta <fabiogustavo@tuta.io>
2023-08-20 15:41:24 -0400

    removendo getters

--- src/funcionario.py
+++ src/funcionario.py
@@ -7,14 +7,8 @@ class Funcionario(Pessoa):
         self._cargo = customtkinter.StringVar()
         self._salario = customtkinter.StringVar()
 
-    def getCargo(self):
-        return self._cargo
-
     def setCargo(self, cargo):
         self._cargo = cargo
 
-    def getSalario(self):
-        return self._salario
-    
     def setSalario(self, salario):
         self._salario = salario
