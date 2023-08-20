80b5b680f115fcedd9eaf6ff9427c7436a66ebd1
Bruno Gomes <brunoedua21@gmail.com>
2023-08-20 16:37:22 -0300

    Retirando os getters

--- src/pessoa.py
+++ src/pessoa.py
@@ -12,56 +12,29 @@ class Pessoa:
         self._telefone = customtkinter.StringVar()
         self._email = customtkinter.StringVar()
 
-    def getCpf(self):
-        return self._cpf.get()
-
     def setCpf(self, value):
         self._cpf = value
-
-    def getRg(self):
-        return self._rg.get()
 
     def setRg(self, value):
         self._rg = value
 
-    def getNome(self):
-        return self._nome.get()
-
     def setNome(self, value):
         self._nome = value
-
-    def getEndereco(self):
-        return self._endereco.get()
 
     def setEndereco(self, value):
         self._endereco = value
 
-    def getNumero(self):
-        return self._numero.get()
-
     def setNumero(self, value):
         self._numero = value
-
-    def getBairro(self):
-        return self._bairro.get()
 
     def setBairro(self, value):
         self._bairro = value
 
-    def getCep(self):
-        return self._cep.get()
-
     def setCep(self, value):
         self._cep = value
 
-    def getTelefone(self):
-        return self._telefone.get()
-
     def setTelefone(self, value):
         self._telefone = value
-
-    def getEmail(self):
-        return self._email.get()
 
     def setEmail(self, value):
         self._email = value
